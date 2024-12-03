d445d191832ee99cf2f38f9e6b673cbfc3eca22b
BlobOfAwe <jamescvm03@gmail.com>
2024-12-02 20:36:00 -0800

    Patient Consent Menu
    Added a menu option to collect consent from the patient before treatment

--- Assets/Scripts/Interactables/PatientBehaviour.cs
+++ Assets/Scripts/Interactables/PatientBehaviour.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PatientBehaviour : NPCDialogue
 {
+    [Header("ConsentCanvas")]
+    [SerializeField] private Canvas consentCanvas;
+    [SerializeField] private TextMeshProUGUI consentText;
+    public bool consentGiven;
+    private DialogueButton[] consentButtons;
+
     // To be called by other scripts when certain conditions are met so the level can end.
     public void FixPatient()
     {
@@ -11,4 +18,23 @@ public class PatientBehaviour : NPCDialogue
         levelManager.EndLevel();
         Debug.Log("Patient Resolved!");
     }
+
+    public override void OnInteract()
+    {
+        if (consentGiven) { base.OnInteract(); }
+        else
+        {
+            consentButtons = consentCanvas.GetComponentsInChildren<DialogueButton>();
+            consentCanvas.gameObject.SetActive(true); // Enable the canvas
+            foreach (DialogueButton button in consentButtons)
+            {
+                button.gameObject.SetActive(button.available);
+            } // Enable each of the dialogue buttons under the canvas
+            consentText.text = ""; // Delete any text from the responseText object
+            consentText.gameObject.SetActive(false); // Hide the responseText object
+        }
+    }
+
+    public void GiveConsent(bool consenting)
+    { consentGiven = consenting; }
 }
